9d0af934cbb0cef36590efca54404feac6c75720
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2022-01-05 17:08:13 +0100

    Advancing in data gerator

--- Inv-MR-Sort/data_generator.py
+++ Inv-MR-Sort/data_generator.py
@@ -1,13 +1,69 @@
+"""
+This file generates the data for the Inverse MR-Sort problem.
+
+"""
+import pandas as pd
+
+def parameter_verification(weights: list(int), profiles: list(int), lmbda: int, n:int, p:int) -> bool:
+    """
+    Verifies if the parameters are correct.
+
+    Arguments:
+        weights: list(int) -- list of weights
+        profiles: list(int) -- list of profiles
+        lmbda: int -- lambda value
+        n: int -- number of criterias
+        p: int -- number of profiles
+    Returns:
+        bool -- True if the parameters are correct, False otherwise
+    """
+    if len(weights) != p:
+        return False
+    if len(profiles) != n:
+        return False
+    if lmbda < 0:
+        return False
+    return True
+
+def generate(n_generated:int, weights: list(int), profiles: list(int), lmbda: int) -> pd.DataFrame:
+    """
+    Generates the data for the Inverse MR-Sort problem.
+
+    Arguments:
+        n_generated: Number of generated instances.
+        weights: list(int) -- list of weights
+        profiles: list(int) -- list of profiles
+        lmbda: int -- lambda value
+    Returns:
+        pd.DataFrame -- dataframe with the generated data
+    """
+    p = len(profiles)
+    n = len(weights)
+    data = pd.DataFrame(columns=['note', 'lmbda'])
+    for student_id in range(n_generated):
+        
+        
+    return 
+
+def save_csv(df: pd.DataFrame, filename: str) -> None:
+    """
+    Save the dataframe to a csv file.
+    """
+    df.to_csv(filename, index=False)
 
 
+if __name__ == "__main__":
 
-
-
-
-
-
-
-
-
-if name == "__main__":
-    main()
+    # Parameters
+    n = 10  # Number of criteria
+    p = 2 # number of profiles (the classe "no classe" is not counted)
+    weights = [0.08, 0.08, 0.08, 0.08, 0.08, 0.12, 0.10, 0.14, 0.14, 0.10]
+    profiles = [[10, 12, 10, 12, 8, 13, 11, 13, 14, 14],
+                [12, 14, 10, 13, 9, 17, 13, 15, 17, 19]]
+    lmbda = 0.6
+    n_generated = 100
+    # generate
+    data = generate(n_generated, weights, profiles, lmbda)
+    
+    save_csv(data, "data.csv")
+    
--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # This is a project for decision systems
 
 Please feel free to read the file `MR-Sort-NCS.pdf` to understand the case.
+
+
+
+# Data Structure :
+Each instance of the problems should be stroed in a csv file with the following format:
+| id  |  note_1  |  note_2  |      classe   |
+|---- |----|----|----|
+|  0  |  12  |  16  |     1     |
+|  1  |  18  |  2  |     0    |
+|  2  |  18  |  10  |     1    |
+
+
+
+w_1 w_2 w_3 
+
+
+lambda
+w_1 note_1
+w_2 note_2
+w_3 note_3
+w_4 note_4
+
+b_1 b_2 b_3 b_4
+
+sum(weights) = 1
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+sum(weights, tq w_i>=b_i) >= lambda
+
+
+
+b_1 = 12
+note_1 = 19 12 15 16 => w_1
+note_1 = 4 7 3 => 0
